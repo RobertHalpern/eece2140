97ce5fed95c7cd42b7c154d580b4319ea621d7cc
rhalpern4040@gmail.com <rhalpern4040@gmail.com>
2025-10-24 12:48:37 -0400

    Initial commit for main, delegating tasks to other classes for now

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import physics
-import visuals
+import visuals # currently no visuals.py so won't do anything
 
 # Try to follow this signature if possible. It's good to have a main file within each of your files
 # so you can call other helpers from that (i.e runPhysicsCalc should call helpers)
 
-def runGame(initial_speed, initial_angle):
+def run_game(initial_speed, initial_angle):
     # Delegate to physics.py
-    arc = physics.runPhysicsCalc(initial_speed, initial_angle)
+    # phyics has constants declared already for ground level and gravity.
+    arc = physics.physics_calc(initial_speed, initial_angle)
 
     # Delegate to visuals.py
     visuals.runVisuals(arc)
@@ -24,7 +25,7 @@ def main():
         return
 
     # Run the main simulation
-    runGame(initial_speed, initial_angle)
+    run_game(initial_speed, initial_angle)
 
 
 if __name__ == "__main__":
