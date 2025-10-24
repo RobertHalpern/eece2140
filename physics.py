e288ac6cc4c6afddd3a49721e865d0697651aa91
Nathaniel Evans <88197135+Ndogg4106@users.noreply.github.com>
2025-10-24 12:29:05 -0400

    Initial commit for physics

--- physics.py
+++ physics.py
@@ -1,3 +1,14 @@
-# initial commit test
+#Math
 
-print("Physics program")
+import math
+
+def PhysicsCalc(Velocity, Angle):
+    # Convert angle from degrees to radians
+    Angle_rad = math.radians(Angle)
+    
+    # Calculate horizontal and vertical components of velocity
+    Vx = Velocity * math.cos(Angle_rad)
+    Vy = Velocity * math.sin(Angle_rad)
+    
+    
+   
